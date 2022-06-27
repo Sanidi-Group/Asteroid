7b941148119dba2988497e9fdb3faf113c21af0c
Manu254rce <straussmoabit@gmail.com>
2022-06-28 01:47:47 +0300

    ...

--- asteroid/src/App.vue
+++ asteroid/src/App.vue
@@ -1,8 +1,16 @@
+<script lang="ts">
+  import Header from './components/Header.vue'
+
+  export default{
+
+    components: 
+    {
+      Header
+    }
+  }
+
+</script>
+
 <template>
-  <div class="bg-slate-900 h-screen">
-    <h1 class="text-center text-6xl py-52 
-            text-white text-shadow-lg shadow-rose-600 font-comfortaa">
-        asteroid
-    </h1>
-  </div>
+  <Header/>
 </template>
---
+++ asteroid/src/components/Header.vue
@@ -0,0 +1,11 @@
+<template>
+
+  <header class="bg-slate-900 h-screen">
+    <h1 class="text-center text-7xl py-52 
+            text-white text-shadow-lg shadow-rose-600 font-comfortaa">
+        asteroid
+    </h1>
+    <div class="absolute top-[40%] left-[50%]
+                w-1/4 h-24 glassDiv"></div>
+  </header>
+</template>
